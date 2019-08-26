27577d6dc2c0e38caaa414629d62b95092f28232
Richard Carranza <rishardcarranza@gmail.com>
2019-08-25 22:08:55 -0600

    Added movie detail

--- src/app/components/detalle/detalle.component.ts
+++ src/app/components/detalle/detalle.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { MoviesService } from '../../services/movies.service';
+import { MovieDetail } from '../../interfaces/interfaces';
 
 @Component({
   selector: 'app-detalle',
@@ -9,14 +10,17 @@ import { MoviesService } from '../../services/movies.service';
 export class DetalleComponent implements OnInit {
 
     @Input() id;
+    pelicula: MovieDetail = {};
+    ocultar = 150;
 
   constructor(private movieService: MoviesService) { }
 
   ngOnInit() {
-    // console.log('ID', this.id);
+    console.log('ocultar', this.ocultar);
     this.movieService.getPeliculaDetalle(this.id)
         .subscribe(resp => {
             console.log('Pelicula Detalle', resp);
+            this.pelicula = resp;
         });
 
     this.movieService.getActoresPelicula(this.id)
