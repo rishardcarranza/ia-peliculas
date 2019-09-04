d0886865d0395977f4ff5faa11f8cea9d910c007
Ricardo Carranza <ricardo.carranza@tegraglobal.com>
2019-09-04 09:34:29 -0600

    Section 9 finish. Aplicacion Peliculas

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { MovieResult, MovieDetail, Credits } from '../interfaces/interfaces';
+import { MovieResult, MovieDetail, Credits, Genre } from '../interfaces/interfaces';
 import { environment } from '../../environments/environment';
+import { resolve } from 'dns';
 
 const URL = environment.url;
 const apiKey = environment.apiKey;
@@ -13,6 +14,7 @@ const apiKey = environment.apiKey;
 export class MoviesService {
 
     private popularesPage = 0;
+    generos: Genre[] = []; 
 
   constructor(private http: HttpClient) { }
 
@@ -57,4 +59,19 @@ export class MoviesService {
   buscarPelicula(texto: string) {
     return this.ejecutarQuery(`/search/movie?query=${texto}`);
   }
+
+  cargarGeneros(): Promise<Genre[]> {
+    return new Promise(resolve => {
+
+        this.ejecutarQuery(`/genre/movie/list?a=1`)
+        .subscribe(resp => {
+            this.generos = resp['genres'];
+            // console.log(this.generos);
+
+            resolve(this.generos);
+        });
+
+    });
+  }
+
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MovieDetail, Genre } from '../interfaces/interfaces';
+import { DataLocalService } from '../services/data-local.service';
+import { MoviesService } from '../services/movies.service';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
+export class Tab3Page implements OnInit {
 
-  constructor() {}
+    peliculas: MovieDetail[] = [];
+    generos: Genre[] = [];
+
+    favoritoPorGenero: any[] = [];
+
+  constructor(private dataLocal: DataLocalService,
+              private movieService: MoviesService) {}
+
+  async ngOnInit() {}
+
+  /**
+   * Ciclo de vida de los componentes
+   * ionViewWillEnter: Se ejecuta antes de que se muestre en pantalla la pagina
+   */
+  async ionViewWillEnter() {
+    this.peliculas = await this.dataLocal.loadFavorites();
+    this.generos = await this.movieService.cargarGeneros();
+
+    // console.log(this.peliculas);
+    this.peliculasPorGenero(this.generos, this.peliculas);
+  }
+
+  peliculasPorGenero(generos: Genre[], peliculas: MovieDetail[]) {
+
+    // Obtener las peliculas en base al genero y agruparlas en base a este
+    this.favoritoPorGenero = [];
+    generos.forEach(genero => {
+        this.favoritoPorGenero.push({
+            genero: genero.name,
+            peliculas: peliculas.filter(peli => {
+                            return peli.genres.find(genre => genre.id === genero.id);
+                        })
+        });
+    });
+
+    console.log(this.favoritoPorGenero);
+  }
 
 }
